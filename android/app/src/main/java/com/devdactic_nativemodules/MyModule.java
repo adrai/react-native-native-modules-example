9085fb09a6cb1e889c200b1937220263b32844c3
Adriano Raiano <adriano@raiano.ch>
2016-12-15 11:02:22 +0100

    first commit

---
+++ android/app/src/main/java/com/devdactic_nativemodules/MyModule.java
@@ -0,0 +1,44 @@
+package com.directory2;
+
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.NativeModule;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class MyModule extends ReactContextBaseJavaModule {
+
+  public MyModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  // Available as NativeModules.MyCustomModule.processString
+  @ReactMethod
+  public void processString(String input, Callback callback) {
+      callback.invoke("Ciao Adriano " + input);
+  }
+
+  // Available as NativeModules.MyCustomModule.squareMe
+  @ReactMethod
+  public void squareMe(int input, Callback callback) {
+      callback.invoke(null, input * input);
+  }
+
+  @Override
+  public String getName() {
+    return "MyModule";
+  }
+
+  @Override
+  public Map<String, Object> getConstants() {
+    final Map<String, Object> constants = new HashMap<>();
+    constants.put("greeting", "Welcome to the DevDactic\n React Native (Android) Tutorial, right?!");
+    return constants;
+  }
+}
---
+++ shared.js
@@ -0,0 +1,85 @@
+var React = require('react');
+var ReactNative = require('react-native');
+
+// 1
+var {
+  Component
+} = React;
+var {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} = ReactNative;
+
+// 2
+var MyModule = require('NativeModules').MyModule;
+
+class devdactic_nativeModules extends Component {
+  // 3
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 0
+    };
+  }
+
+  // 4
+  render() {
+    return (
+      <View style={styles.container}>
+      <Text style={styles.welcome}>
+      {MyModule.greeting}
+      </Text>
+      <TextInput style={styles.input} onChangeText={(text) => this.squareMe(text)}/>
+      <Text style={styles.result}>
+      {this.state.number}
+      </Text>
+      </View>
+    );
+  }
+
+  // 5
+  squareMe(num) {
+    if (num == '') {
+      return;
+    }
+    MyModule.squareMe(parseInt(num, 10), (error, number) => {
+      if (error) {
+        console.error(error);
+      } else {
+        this.setState({number: number});
+      }
+    })
+  }
+};
+
+// 6
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 20,
+  },
+  input: {
+    width: 100,
+    height: 40,
+    borderColor: 'red',
+    borderWidth: 1,
+    alignSelf: 'center'
+  },
+  result: {
+    textAlign: 'center',
+    color: '#333333',
+    fontSize: 30,
+    fontWeight: 'bold',
+    margin: 20,
+  },
+});
+
+module.exports = devdactic_nativeModules;
